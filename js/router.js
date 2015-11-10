0d2b05bb8fcbf95f8204c8c6d5086251019bf821
Ryan Ciecalone <ryan@ryanchristianmedia.com>
2015-11-10 10:45:26 -0700

    Updated Login with delete functionality

---
+++ js/router.js
@@ -0,0 +1,38 @@
+app.config(function ($routeProvider) {
+ $routeProvider
+
+    .when('/', {
+     templateUrl: 'pages/home.html',
+     controller:'mainController',
+   })
+
+   .when('/login', {
+     templateUrl: 'pages/auth/login.html',
+     controller:'mainController',
+   })
+
+    .when('/signup', {
+     templateUrl: 'pages/auth/signUp.html',
+     controller:'mainController',
+   })
+
+     .when('/updateinfo', {
+     templateUrl: 'pages/auth/updateEmailAndPass.html',
+     controller:'mainController',
+   })
+
+       .when('/resetpassword', {
+     templateUrl: 'pages/auth/resetpassword.html',
+     controller:'mainController',
+   })
+
+   .when('/deleteaccount', {
+     templateUrl: 'pages/auth/deleteAccount.html',
+     controller:'mainController',
+   })
+
+   .otherwise({redirectTo: '/'});
+});
+
+
+
