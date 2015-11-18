8c446635fca9e4d259eb7a0a146d0ebd455ef71a
Patrick <pcnicklaus@gmail.com>
2015-11-17 17:23:12 -0700

    got a little further ont he quering thing and filling one of the slots - not dynamic but still, a step further

--- js/controllers/calendarController.js
+++ js/controllers/calendarController.js
@@ -169,20 +169,32 @@ $scope.callBack = function(){
   var inEvents;
   var ref = new Firebase("https://momsmorningscheduler.firebaseio.com/events");
   ref.on("value", function(snapshot) {
+    snapshot.forEach(function (childSnapshot){
+      var key = childSnapshot.key();
+        console.log(key, " key");
+      var childData = childSnapshot.val();
+      console.log(childData, ' key')
+       if (dateChosen === childData.date) {
+          var ref2 = new Firebase("https://momsmorningscheduler.firebaseio.com/events/" + key + '/slots');
+          ref2.update({
+            0: true
+          });
+      }
+
+
+    });
   events2 = snapshot.exportVal();
-  console.log(events2);
-  for(var key in events2){
+  // console.log(events2);
+  // for(var key in events2){
 
-     inEvents = events2[key];
+  //    inEvents = events2[key];
 
-     if (dateChosen === inEvents.date) {
-      console.log(inEvents);
-      console.log(inEvents.date);
-      console.log( 'wah-hoo');
-
-
-     }
-  }
+     // if (dateChosen === inEvents.date) {
+     //  console.log(inEvents);
+     //  console.log(inEvents.date);
+     //  console.log( 'wah-hoo');
+     // }
+  // }
 
   $rootScope.events2 = events2;
 }, function (errorObject) {
